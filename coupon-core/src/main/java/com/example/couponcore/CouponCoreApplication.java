f2a66e3d6a5775df2682056632913ef7897d85ae
twoosky <lhjae3@naver.com>
2023-06-08 06:15:51 +0900

    fix: main 함수 제거

--- coupon-core/src/main/java/com/example/couponcore/CouponCoreApplication.java
+++ coupon-core/src/main/java/com/example/couponcore/CouponCoreApplication.java
@@ -1,10 +1,16 @@
 package com.example.couponcore;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan(basePackageClasses = {CouponCoreApplication.class})
 @Configuration
+@ConfigurationPropertiesScan
+@EntityScan(basePackages = {"com.example.couponcore.domain"})
+@EnableJpaRepositories(basePackages = {"com.example.couponcore.repository"})
 public class CouponCoreApplication {
 
 }
