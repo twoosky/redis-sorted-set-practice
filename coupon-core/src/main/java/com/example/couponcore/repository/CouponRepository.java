1c532f77d8b7e051dc62d64b8b4030be200091e4
twoosky <lhjae3@naver.com>
2023-06-08 06:10:46 +0900

    chore: core 모듈 의존성 추가

--- coupon-core/src/main/java/com/example/couponcore/repository/CouponRepository.java
+++ coupon-core/src/main/java/com/example/couponcore/repository/CouponRepository.java
@@ -3,9 +3,11 @@ package com.example.couponcore.repository;
 import com.example.couponcore.domain.CouponPolicy;
 import com.example.couponcore.domain.vo.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long> {
     Optional<CouponPolicy> findByEvent(Event event);
 }
