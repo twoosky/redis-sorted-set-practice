97c1ca21c6cf4b370a35815744e5d75f7a43200d
twoosky <lhjae3@naver.com>
2023-06-08 06:15:39 +0900

    feat: redis Set 구조 메서드 작성

--- coupon-core/src/main/java/com/example/couponcore/repository/RedisRepository.java
+++ coupon-core/src/main/java/com/example/couponcore/repository/RedisRepository.java
@@ -2,17 +2,20 @@ package com.example.couponcore.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Repository
 @RequiredArgsConstructor
 public class RedisRepository {
     private final RedisTemplate<String, Object> redisTemplate;
+    private static final String SET_BASE_KEY = "issued-";
 
     public Boolean zAdd(String key, Object value, Double score) {
-        return redisTemplate.opsForZSet().addIfAbsent(key, value, score);
+        return redisTemplate.opsForZSet().add(key, value, score);
     }
 
     public Set<Object> zPop(String key, Long count) {
@@ -34,14 +37,14 @@ public class RedisRepository {
     }
 
     public Long sAdd(String key, Object value) {
-        return redisTemplate.opsForSet().add(key, value);
+        return redisTemplate.opsForSet().add(SET_BASE_KEY + key, value);
     }
 
     public Long sRem(String key, Object value) {
-        return redisTemplate.opsForSet().remove(key, value);
+        return redisTemplate.opsForSet().remove(SET_BASE_KEY + key, value);
     }
 
     public Long sCard(String key) {
-        return redisTemplate.opsForSet().size(key);
+        return redisTemplate.opsForSet().size(SET_BASE_KEY + key);
     }
 }
